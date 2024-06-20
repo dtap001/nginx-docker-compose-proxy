22f9555d9e47e8f06fc4f9298499aac77b97de96
dtap <tapai.david@cae.com>
2024-06-20 10:38:09 +0200

    feat: https support

--- startup.sh
+++ startup.sh
@@ -5,5 +5,11 @@ if [ -z "$DESTINATION_HTTP_PORT" ]; then
   exit 1
 fi
 
-envsubst '$DESTINATION_HTTP_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+if [ -z "$DESTINATION_HTTPS_PORT" ]; then
+  echo "Error: DESTINATION_HTTPS_PORT is not set."
+  exit 1
+fi
+
+envsubst '${DESTINATION_HTTP_PORT} ${HTTPS_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
 exec "$@"
